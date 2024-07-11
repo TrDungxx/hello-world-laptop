1505241704f40e4c83c6d1f1d44ca6128f3ddffe
TrDungxx <Hovercard27@gmail.com>
2024-07-11 15:56:37 +0700

    reacJS laptop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Run1 from "./component/run1";
+
+
+const App = () => {
+return (
+<div>
+  <div className='Staffservicepage'>
+    
+<Run1 />
+
+ </div>
+ </div>
+);
 }
 
 export default App;
+
---
+++ src/component/run1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./run1.css";
+import "./background.css";
+import Line1 from "./line1";
+import Line2 from "./line2";
+
+
+
+function Run1() {
+  return (
+    <section>
+      <div className="Staffservicepage">
+        <Line1 />
+        <Line2/>
+      </div>
+    </section>
+  );
+}
+export default Run1;
